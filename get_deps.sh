64ffe9d5aa6b87f61224d10f4024bd7eff534d8b
imotai <wangtaize@4paradigm.com>
2020-06-29 19:10:39 +0800

    fix: add fesql lib

--- get_deps.sh
+++ get_deps.sh
@@ -4,6 +4,8 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     wget https://storage.4paradigm.com/api/public/dl/kGmS8y_L/fedb_client_2.0.0.0-beta.tar.gz
     tar -zxvf fedb_client_2.0.0.0-beta.tar.gz
     pip install -U fedb_client_2.0.0.0-beta/fedb-2.0.0.0-cp37-cp37m-macosx_10_9_x86_64.whl
+    wget https://storage.4paradigm.com/api/public/dl/T9bEk8ho/fesql-0.1.0.0-cp37-cp37m-macosx_10_9_x86_64.whl
+    pip install -U fesql-0.1.0.0-cp37-cp37m-macosx_10_9_x86_64.whl
 else
     echo "todo"
 fi
