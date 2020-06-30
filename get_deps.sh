4145f32bfdf47a7e86ffc771d0b75787e9236975
imotai <wangtaize@4paradigm.com>
2020-06-30 11:26:45 +0800

    feat: add py4j and linux sdk

--- get_deps.sh
+++ get_deps.sh
@@ -7,6 +7,10 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     wget https://storage.4paradigm.com/api/public/dl/T9bEk8ho/fesql-0.1.0.0-cp37-cp37m-macosx_10_9_x86_64.whl
     pip install -U fesql-0.1.0.0-cp37-cp37m-macosx_10_9_x86_64.whl
 else
-    echo "todo"
+    wget https://storage.4paradigm.com/api/public/dl/hPOAsawT/fedb_client_linux_2.0.0.0-beta.tar.gz
+    tar -zxvf fedb_client_linux_2.0.0.0-beta.tar.gz
+    pip install -U fedb_client_linux_2.0.0.0-beta/fedb-2.0.0.0-cp37-cp37m-linux_x86_64.whl
+    wget https://storage.4paradigm.com/api/public/dl/TtqSKBUu/fesql-0.1.0.0-cp37-cp37m-linux_x86_64.whl
+    pip install -U fesql-0.1.0.0-cp37-cp37m-linux_x86_64.whl
 fi
 
