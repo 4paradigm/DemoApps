19694c26a1da651c6ae36900528a8b473e0718d2
imotai <wangtaize@4paradigm.com>
2021-05-27 15:23:25 +0800

    fix: update notebook tour

--- predict-taxi-trip-duration-nb/start_demo.sh
+++ predict-taxi-trip-duration-nb/start_demo.sh
@@ -2,5 +2,5 @@
 #
 # start_demo.sh
 
-docker run -h=`hostname`  --network=host -t 4pdosc/fedb_notebook:0.2.0 jupyter notebook
+docker run -h=`hostname`  --network=host -t 4pdosc/fedb_notebook:0.4.0 jupyter notebook
 
